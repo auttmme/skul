1e5751e6e8ead306555cdae630c65813a8074839
auttmme <ichautm14@gmail.com>
2022-11-26 22:19:36 +0700

    import sidebar component to home page

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Sidebar from "../components/sidebar";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default Home;
