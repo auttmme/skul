e17ad0d22bdc476c4e6940e29bc11ea9f1008770
auttmme <ichautm14@gmail.com>
2022-12-04 00:58:47 +0700

    feat(search): create search bar

---
+++ components/home/search.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+
+function Search() {
+  return (
+    <Flex w={"full"} justifyContent={"center"}>
+      <InputGroup w={"4xl"}>
+        <InputRightElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.300" />}
+        />
+        <Input type="tel" placeholder="Search" />
+      </InputGroup>
+    </Flex>
+  );
+}
+
+export default Search;
