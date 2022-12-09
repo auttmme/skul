7a0b54f60bf168a9a76489722d079a402870705e
auttmme <ichautm14@gmail.com>
2022-12-09 17:22:51 +0700

    feat(login): add useState hooks for username and password

--- pages/login.js
+++ pages/login.js
@@ -13,16 +13,32 @@ import Link from "next/link";
 function Login() {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  console.log(`${username} ${password}`);
+  const test = false;
+
+  console.log("test", !test);
+  // test, true
+  // if username length > 1 && password.length > 1 disabled false
   return (
     <Flex h="100vh" alignItems="center" justifyContent="center">
       <Box display="flex" flexDirection={"column"}>
-        <Input placeholder="username" size={"lg"} w={"2xl"} />
+        <Input
+          placeholder="username"
+          size={"lg"}
+          w={"2xl"}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <InputGroup size="lg" marginY={"3"}>
           <Input
             pr="4.5rem"
             type={show ? "text" : "password"}
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <InputRightElement width="4.5rem">
             <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -32,7 +48,12 @@ function Login() {
         </InputGroup>
         <Box alignSelf={"flex-end"}>
           <Link href={"/home"} passHref>
-            <Button>Login</Button>
+            <Button
+              disabled={username.length < 1 || password.length < 1}
+              onClick={() => alert(`${username} ${password}`)}
+            >
+              Login
+            </Button>
           </Link>
         </Box>
       </Box>
