ba2986eb124688470353643d8f91df93a8abbe77
auttmme <ichautm14@gmail.com>
2022-12-09 18:32:16 +0700

    fix(home): move import icon to before import Layout & remove unnecessary curly bracket in flex property

--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,17 @@
 import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import { FiFilter } from "react-icons/fi";
 import Layout from "../components/common/layout";
 import Search from "../components/home/search";
 import DataTable from "../components/home/table";
-import { FiFilter } from "react-icons/fi";
 
 function Home() {
   return (
     <Box>
       <Layout>
-        <Flex padding={"8px"} mb={"16px"}>
+        <Flex padding="8px" mb="16px">
           <Search />
-          <FiFilter fontSize={"24px"} />
+          <FiFilter fontSize="24px" />
         </Flex>
         <DataTable />
       </Layout>
