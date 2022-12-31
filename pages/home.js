f80546c473e47630a1d69fd0a91077403e59778b
auttmme <ichautm14@gmail.com>
2022-12-31 14:32:49 +0700

    feat(home & useFetch): fetch data from stein

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,16 @@
-import { Box, Flex } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Flex } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import { FiFilter } from "react-icons/fi";
+import useFetch from "utils/useFetch";
 import Layout from "/components/common/layout";
 import Search from "/components/home/search";
 import DataTable from "/components/home/table";
 
 function Home() {
+  const { data, isPending, error } = useFetch(
+    "https://api.steinhq.com/v1/storages/63a5d577eced9b09e9ac9bcc"
+  );
+
   return (
     <Box>
       <Layout>
@@ -13,7 +18,7 @@ function Home() {
           <Search />
           <FiFilter fontSize="24px" />
         </Flex>
-        <DataTable />
+        {data && <DataTable pending={isPending} data={data} />}
       </Layout>
     </Box>
   );
