8d3e3a791e43ae26bfe790f6afc3298f23a61f54
auttmme <ichautm14@gmail.com>
2023-03-05 00:31:59 +0700

    breaking-change(inputData): change getStaticProps to getServerSideProps

--- pages/inputData.js
+++ pages/inputData.js
@@ -4,16 +4,16 @@ import FormData from "/components/inputData/formData";
 
 function InputData({ area }) {
   console.log("area:", area !== null);
-  return <Layout>{area !== null > 0 && <FormData area={area} />}</Layout>;
+  return <Layout>{area && <FormData area={area} />}</Layout>;
 }
 
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/api/area");
+export async function getServerSideProps() {
+  const res = await fetch("http://127.0.0.1:3000/api/area");
   const area = await res.json();
 
   return {
     props: {
-      area: area.area,
+      area: area?.area,
     },
   };
 }
